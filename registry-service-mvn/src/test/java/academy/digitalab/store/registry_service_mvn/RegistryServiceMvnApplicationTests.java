3b30c039853abc5736a3e2bd134bca9006befa69
Willmer Orsetti <112994224+willmerorsetti@users.noreply.github.com>
2025-02-10 22:26:52 -0600

    Add files via upload
    Codigo ejemplo servicio eureka con spring boot funcionado

---
+++ registry-service-mvn/src/test/java/academy/digitalab/store/registry_service_mvn/RegistryServiceMvnApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.digitalab.store.registry_service_mvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceMvnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
